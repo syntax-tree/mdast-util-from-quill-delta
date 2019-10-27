56edcfeefebd32defafb28d076a805d7eb18cc75
Christian Murphy <Christian.Murphy.42@gmail.com>
2019-10-26 20:04:25 -0700

    feat: add support for bold, italic, and strike through

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,47 @@
 import test from 'ava'
 import Delta from 'quill-delta'
+import u from 'unist-builder'
 import deltaToMdast from '.'
 
-test('can pass a delta', t => {
-  const delta = new Delta()
-  t.deepEqual(deltaToMdast(delta), {type: 'root'})
+test('should handle empty delta', t => {
+  const delta = new Delta([])
+  t.deepEqual(deltaToMdast(delta), u('root', []))
+})
+
+test('should handle plain test', t => {
+  const delta = new Delta([{insert: 'plain text'}])
+  t.deepEqual(deltaToMdast(delta), u('root', [u('text', 'plain text')]))
+})
+
+test('should handle bold', t => {
+  const delta = new Delta([{insert: 'bolded', attributes: {bold: true}}])
+  t.deepEqual(
+    deltaToMdast(delta),
+    u('root', [u('strong', [u('text', 'bolded')])])
+  )
+})
+
+test('should handle italic', t => {
+  const delta = new Delta([{insert: 'italicized', attributes: {italic: true}}])
+  t.deepEqual(
+    deltaToMdast(delta),
+    u('root', [u('emphasis', [u('text', 'italicized')])])
+  )
+})
+
+test('should handle strike through', t => {
+  const delta = new Delta([
+    {insert: 'strike through', attributes: {strike: true}}
+  ])
+  t.deepEqual(
+    deltaToMdast(delta),
+    u('root', [u('delete', [u('text', 'strike through')])])
+  )
+})
+
+test('should not handle custom content types, yet', t => {
+  const delta = new Delta([
+    {insert: {image: 'https://quilljs.com/assets/images/icon.png'}}
+  ])
+  t.throws(() => deltaToMdast(delta))
 })
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
 import Delta from 'quill-delta'
 import u from 'unist-builder'
+import Op from 'quill-delta/dist/Op'
+import {Root, Content} from 'mdast'
 
-export = (delta: Delta) => {
-  console.log(delta)
-  return u('root')
+const quillAttributeMapping = ({insert, attributes}: Op): Content => {
+  if (typeof insert === 'string') {
+    let content: Content = u('text', insert)
+
+    if (typeof attributes === 'undefined') {
+      return content
+    }
+
+    if ('bold' in attributes) {
+      content = u('strong', [content])
+    }
+
+    if ('italic' in attributes) {
+      content = u('emphasis', [content])
+    }
+
+    if ('strike' in attributes) {
+      content = u('delete', [content])
+    }
+
+    return content
+  }
+
+  throw new TypeError('unable to process operation')
 }
+
+const quillDeltaToMdast = (delta: Delta): Root => {
+  return u('root', delta.ops.map(quillAttributeMapping))
+}
+
+export = quillDeltaToMdast
